22a3b7d4259ddbd745cd3a26b1bb9b0d283e045e
Aleksey <cs-zatopija@mail.ru>
2020-03-03 19:38:32 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    let lengthOfString = str.length;
+    let arrayOfStrings = bracketsConfig.map((item) => item.join(''));
+
+    for (let i = 1; i <= lengthOfString; ++i) {
+        for (let j = 0; j <= arrayOfStrings.length; ++j) {
+            str = str.replace(arrayOfStrings[j], '');
+        }
+    }
+
+    let result = str.length === 0 ? true : false;
+    return result;
+};
